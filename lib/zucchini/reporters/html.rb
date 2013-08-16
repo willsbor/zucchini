77652cf20197d3ef29eb9d05f15710564481e1b5
Vasily Mikhaylichenko <vasily.mikhaylichenko@gmail.com>
2013-08-17 03:46:07 +1000

    Write relative assets and images paths to the HTML report

--- lib/zucchini/reporters/html.rb
+++ lib/zucchini/reporters/html.rb
@@ -1,35 +1,33 @@
 require 'time'
 require 'erb'
 require 'fileutils'
+require 'pathname'
 
 module Zucchini::Reporter
   class HTML
-    def self.generate(features, output_path, ci)
-      template_path = File.expand_path("#{File.dirname(__FILE__)}/html/template.erb.html")
-      Zucchini::Reporter::HTML.new(features, ci, template_path, output_path).write!
-
-      "HTML report generated to #{output_path}"
+    def self.generate(features, report_path, ci)
+      Zucchini::Reporter::HTML.new(features, ci).write(report_path)
+      "HTML report generated to #{report_path}"
     end
 
-    def initialize(features, ci, template_path, output_path)
-      files_path = output_path.chomp(File.extname(output_path)) + '_files'
-
-      @features      = features
-      @device        = features[0].device
-      @time          = Time.now.strftime("%T, %e %B %Y")
-      @assets_path   = "#{files_path}/assets"
-      @images_path   = "#{files_path}/images"
-      @output_path   = output_path
-      @template_path = template_path
-      @ci            = ci ? 'ci' : ''
+    def initialize(features, ci)
+      @features = features
+      @device   = features[0].device
+      @time     = Time.now.strftime("%T, %e %B %Y")
+      @ci       = ci ? 'ci' : ''
     end
 
-    def write!
-      copy_assets(File.expand_path("#{File.dirname(__FILE__)}/html"), @assets_path)
-      copy_result_images(@features, @images_path)
+    def write(report_path)
+      template_path  = "#{File.dirname(__FILE__)}/html/template.erb.html"
+      gem_assets_dir = "#{File.dirname(__FILE__)}/html"
 
-      File.open(@output_path, 'w+') do |f|
-        f.write ERB.new(File.read(@template_path)).result(binding)
+      files_path = report_path.chomp(File.extname report_path) + '_files'
+
+      @assets_path = copy_assets(gem_assets_dir, "#{files_path}/assets", report_path)
+      @features    = copy_images(@features,      "#{files_path}/images", report_path)
+
+      File.open(report_path, 'w+') do |f|
+        f.write ERB.new(File.read(template_path)).result(binding)
       end
     end
 
@@ -40,18 +38,22 @@ module Zucchini::Reporter
       FileUtils.mkdir_p path
     end
 
-    def copy_assets(src_dir, dest_dir)
-      recreate_dir(dest_dir)
-      %W(js css).each do |type|
-        FileUtils.cp_r("#{src_dir}/#{type}", dest_dir)
-      end
+    def relative_path(dest_path, base_path)
+      Pathname.new(dest_path).relative_path_from(Pathname.new(base_path).dirname)
     end
 
-    def copy_result_images(features, dest_dir)
+    def copy_assets(src_dir, dest_dir, report_path)
+      recreate_dir(dest_dir)
+      %W(js css).each { |t| FileUtils.cp_r("#{src_dir}/#{t}", dest_dir) }
+
+      relative_path(dest_dir, report_path)
+    end
+
+    def copy_images(features, dest_dir, report_path)
       recreate_dir(dest_dir)
 
       features.each do |f|
-        f.screenshots.each do |s|
+        f.screenshots.map do |s|
           %W(actual expected difference).each do |type|
             src_path = s.result_images[type.to_sym]
             if src_path
@@ -62,11 +64,12 @@ module Zucchini::Reporter
               FileUtils.mkdir_p(type_dir)
               FileUtils.cp(src_path, dest_path)
 
-              s.result_images[type.to_sym] = dest_path
+              s.result_images[type.to_sym] = relative_path(dest_path, report_path)
             end
           end
         end
       end
     end
+
   end
 end
