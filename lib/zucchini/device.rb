01524a174c7057208c1c35ec32168adf34f2adb1
Thomas Weese <tweese@ebay.com>
2014-04-13 20:48:27 +0200

    removed obsolete set_simulator_device method updated config.yml template

--- lib/zucchini/device.rb
+++ lib/zucchini/device.rb
@@ -7,21 +7,9 @@ module Zucchini
 
     def device_params(device)
       if is_simulator?(device)
-        set_simulator_device(device)
         "-w \"#{device[:simulator]}\""
       else
         "-w #{device[:udid]}"
-      end
-    end
-
-    def set_simulator_device(device)
-      return unless device[:simulator].is_a?(String)
-
-      current_simulated_device = `defaults read com.apple.iphonesimulator "SimulateDevice"`.chomp
-      if current_simulated_device != device[:simulator]
-        simulator_pid = `ps ax|awk '/[i]Phone Simulator.app\\/Contents\\/MacOS\\/iPhone Simulator/{print $1}'`.chomp
-        Process.kill('INT', simulator_pid.to_i) unless simulator_pid.empty?
-        `defaults write com.apple.iphonesimulator "SimulateDevice" '"#{device[:simulator]}"'`
       end
     end
 
