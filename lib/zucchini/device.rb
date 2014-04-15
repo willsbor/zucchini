a2726051e103e3a01a332ffc7a599baaf41975f6
Thomas Weese <tweese@ebay.com>
2014-04-15 09:45:26 +0200

    fixed handling with active running instances of the simulator to stop every active one before running next test (was earlier part of the set_simulator_device method)

--- lib/zucchini/device.rb
+++ lib/zucchini/device.rb
@@ -7,10 +7,16 @@ module Zucchini
 
     def device_params(device)
       if is_simulator?(device)
+        stop_active_simulator()
         "-w \"#{device[:simulator]}\""
       else
         "-w #{device[:udid]}"
       end
+    end
+
+    def stop_active_simulator()
+      simulator_pid = `ps ax|awk '/[i]Phone Simulator.app\\/Contents\\/MacOS\\/iPhone Simulator/{print $1}'`.chomp
+      Process.kill('INT', simulator_pid.to_i) unless simulator_pid.empty?
     end
 
     def is_simulator?(device)
