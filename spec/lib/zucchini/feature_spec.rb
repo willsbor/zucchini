a04b59ad2c4db12d26984c760a6be5dc4ca6d50b
willsborKang <willsbor@gmail.com>
2014-05-26 13:43:08 +0800

    add test code for tomask function

--- spec/lib/zucchini/feature_spec.rb
+++ spec/lib/zucchini/feature_spec.rb
@@ -41,5 +41,65 @@ describe Zucchini::Feature do
         FileUtils.rm_rf("#{feature.path}/pending")
       end
     end
+
+    context "copy screenshots to mask directory" do
+      before do
+        feature.path = './spec/sample_setup/feature_three'
+        feature.device = {screen: 'retina_ios5'}
+      end
+      
+      it "should not create mask image to mask directory if there is no diff" do
+        screenshot_path = "#{feature.path}/run_data/Run\ 1/01_screenshot.png"
+        FileUtils.mkdir_p(File.dirname(screenshot_path))
+        FileUtils.copy_file("./spec/sample_setup/feature_one/reference/retina_ios5/01_sign up_spinner.png", screenshot_path)
+        
+        reference_path = "#{feature.path}/reference/retina_ios5/01_screenshot.png"
+        FileUtils.mkdir_p(File.dirname(reference_path))
+        FileUtils.copy_file("./spec/sample_setup/feature_one/reference/retina_ios5/01_sign up_spinner.png", reference_path)
+
+        feature.approve_tomask
+        (File.exists? "#{feature.path}/masks/retina_ios5/01_screenshot.png").should eq false
+      end
+
+      it "should create mask image to mask directory if there is some diff and there is no original mask image" do
+        screenshot_path = "#{feature.path}/run_data/Run\ 1/01_screenshot.png"
+        FileUtils.mkdir_p(File.dirname(screenshot_path))
+        FileUtils.copy_file("./spec/sample_setup/feature_one/reference/retina_ios5/01_sign up_spinner.png", screenshot_path)
+        
+        reference_path = "#{feature.path}/reference/retina_ios5/01_screenshot.png"
+        FileUtils.mkdir_p(File.dirname(reference_path))
+        FileUtils.copy_file("./spec/sample_setup/feature_one/reference/retina_ios5/01_sign up_spinner_error.png", reference_path)
+
+        feature.approve_tomask
+        (File.exists? "#{feature.path}/masks/retina_ios5/01_screenshot.png").should eq true
+      end
+
+      it "should merge original mask image and new mask image when there is a original mask image" do
+        origin_initial_mask_ref_path = "./spec/sample_setup/feature_one/masks/retina_ios5/01_sign up_spinner.png"
+        initial_mask_path = "#{feature.path}/masks/retina_ios5/01_screenshot.png"
+        FileUtils.mkdir_p(File.dirname(initial_mask_path))
+        FileUtils.copy_file(origin_initial_mask_ref_path, initial_mask_path)
+
+        screenshot_path = "#{feature.path}/run_data/Run\ 1/01_screenshot.png"
+        FileUtils.mkdir_p(File.dirname(screenshot_path))
+        FileUtils.copy_file("./spec/sample_setup/feature_one/reference/retina_ios5/01_sign up_spinner.png", screenshot_path)
+        
+        reference_path = "#{feature.path}/reference/retina_ios5/01_screenshot.png"
+        FileUtils.mkdir_p(File.dirname(reference_path))
+        FileUtils.copy_file("./spec/sample_setup/feature_one/reference/retina_ios5/01_sign up_spinner_error.png", reference_path)
+
+        feature.approve_tomask
+        (File.exists? "#{feature.path}/masks/retina_ios5/01_screenshot.png").should eq true
+
+        diff = compare_image(initial_mask_path, origin_initial_mask_ref_path, "#{feature.path}/01_screenshot.png");
+        (diff[0]).should eq :failed
+      end
+
+      after do
+        FileUtils.rm_rf("#{feature.path}/run_data")
+        FileUtils.rm_rf("#{feature.path}/reference")
+        FileUtils.rm_rf("#{feature.path}/masks")
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,3 +17,24 @@ RSpec.configure do |config|
   config.tty = true
   config.formatter = :doc
 end
+
+def compare_image(file1_path, file2_path, diff_path)
+  compare_version_string = `compare --version`.strip.split[2].slice(/\d+\.\d+\.\d+/)
+
+  if Gem::Version.new(compare_version_string) < Gem::Version.new('6.8.8')
+    compare_command = "compare -metric AE -fuzz 2% -dissimilarity-threshold 1 -subimage-search"
+  else
+    compare_command = "compare -metric AE -fuzz 2% -dissimilarity-threshold 1"
+  end
+
+  out = `#{compare_command} \"#{file1_path}\" \"#{file2_path}\" \"#{diff_path}\" 2>&1`
+  out.chomp!
+  out = out.split("\n")[0]
+
+  if Gem::Version.new(compare_version_string) < Gem::Version.new('6.8.8')
+    @diff = (out == '0') ? [:passed, nil] : [:failed, out]
+  else
+    @diff = ($?.exitstatus == 0) ? [:passed, nil] : [:failed, out]
+  end
+
+end
